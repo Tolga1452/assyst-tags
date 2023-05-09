e3e436bbf67296205472ff3cf8513a6c87b537e3
Tolga1452 <tolgchu-studios@outlook.com>
2023-05-09 20:52:16 +0300

    help message

--- experiment-rollout/index.js
+++ experiment-rollout/index.js
@@ -3,6 +3,6 @@ const lastUpdate = ''; //Unix timestamp as seconds
 async function experimentRollout(command, id) {
     id = id.toLowerCase();
 
-    return data
-    if (!id) return `:x: This feature does not exist, type **\`-t ${command}\`** to see all available features.`;
+    if (!id) return `## Usage\n\`-t ${command} <feature_id>\`\n\n## Available Feature Ids\n${Object.keys(data).map(feature => `\`${feature}\``).join(', ')}\n### Last Update\n<t:${lastUpdate}:R>`;
+    if (!data[id]) return `:x: This feature does not exist, type **\`-t ${command}\`** to see all available features.`;
 };
