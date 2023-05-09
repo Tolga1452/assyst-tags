4b770a151de43b84dbbaad0f9f5ba84f60df0625
Tolga1452 <tolgchu-studios@outlook.com>
2023-05-09 20:32:12 +0300

    fix input

--- experiment-rollout/index.js
+++ experiment-rollout/index.js
@@ -1,7 +1,7 @@
 const lastUpdate = ''; //Unix timestamp as seconds
 
 async function experimentsAPI(command, id) {
-    id = input.toLowerCase();
+    id = id.toLowerCase();
 
     const data = await fetch('https://raw.githubusercontent.com/Wumpus-Central/assyst-tags/main/experiment-rollout/data.js').then(response => response.json());
     return data
