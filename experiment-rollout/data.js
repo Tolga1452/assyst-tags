b6cb99f2f544af185e574da53404bee156408cdb
Tolga1452 <tolgchu-studios@outlook.com>
2023-05-11 18:00:27 +0300

    push recent changes (not completed)

--- experiment-rollout/data.js
+++ experiment-rollout/data.js
@@ -13,24 +13,117 @@
   * 4: In beta/alpha testing
   * 
   * Requirement Types
-  * 0: Max member count
+  * 0: Server must not have features
+  * 1: Maximum member count
+  * 2: Member count range
+  * 
+  * Priority Types
+  * 0: Not started
+  * 1: Rolling out
+  * 2: Rolled out
   */
 
 const data = {
     clyde_ai: {
         rate: 21,
-        experimentType: 0,
-        rolloutType: 0
-    },
-    split_permissions: {
-        rate: 100,
+        ranges: [[0, 1200]],
         experimentType: 0,
         rolloutType: 0,
         requirements: [
             {
                 type: 0,
-                value: 50
+                value: ['COMMUNITY'],
+                rate: 100
+            },
+            {
+                type: 1,
+                value: 100,
+                rate: 11,
+                ranges: [
+                    [0, 100],
+                    [200, 1200]
+                ]
+            },
+            {
+                type: 2,
+                value: [101, 200],
+                rate: 10,
+                ranges: [[0, 1000]]
             }
         ]
+    },
+    split_permissions: {
+        rate: 1,
+        ranges: [[0, 100]],
+        experimentType: 0,
+        rolloutType: 0
+    },
+    channel_summaries: {
+        experimentType: 0,
+        rolloutType: 4
+    },
+    pomelo: {
+        experimentType: 1,
+        rolloutType: 1,
+        priority: [
+            {
+                status: 1,
+                name: 'Verified bots (server count wins name conflicts, other ones will get their discriminator appended)'
+            },
+            {
+                status: 0,
+                name: 'Brands'
+            },
+            {
+                status: 0,
+                name: 'Discord staff'
+            },
+            {
+                status: 0,
+                name: 'Partnered and verified server owners'
+            },
+            {
+                status: 0,
+                name: 'Everyone else (people who bought Nitro up to March 1, 2023 will be prioritized)'
+            }
+        ],
+        notes: [
+            {
+                title: 'Checking Pomelo Usernames',
+                text: 'Checking Pomelo usernames via API has patched and accounts which tried to check usernames will be IP banned from Discord.'
+            }
+        ]
+    },
+    birthday_activites: {
+        rate: 0,
+        experimentType: 1,
+        rolloutType: 0
+    },
+    jamspace: {
+        rolloutType: 3
+    },
+    color_together: {
+        rolloutType: 3
+    },
+    voice_messages_server: {
+        rate: 100,
+        experimentType: 0,
+        rolloutType: 0,
+        requirements: [
+            {
+                type: 1,
+                value: 200,
+                rate: 100
+            }
+        ]
+    },
+    soundboard: {
+        rate: 100,
+        experimentType: 0
+    },
+    server_guide: {
+        rate: 50,
+        experimentType: 0,
+        rolloutType: 0
     }
 };
--- experiment-rollout/index.js
+++ experiment-rollout/index.js
@@ -3,6 +3,37 @@ const lastUpdate = ''; //Unix timestamp as seconds
 async function experimentRollout(command, id) {
     id = id.toLowerCase();
 
-    if (!id) return `## Usage\n\`-t ${command} <feature_id>\`\n\n## Available Feature Ids\n${Object.keys(data).map(feature => `\`${feature}\``).join(', ')}\n### Last Update\n<t:${lastUpdate}:R>`;
-    if (!data[id]) return `:x: This feature does not exist, type **\`-t ${command}\`** to see all available features.`;
+    if (!id) return `## Usage\n\`-t ${command} <feature_id>\`\n\n## Available Feature Ids\n${Object.keys(data).map(id => `\`${id}\``).join(', ')}\n### Last Update\n\n<t:${lastUpdate}:R>`;
+    if (!data[id]) return `:x: This feature id does not exist. Type **\`-t ${command}\`** to see all available feature ids.`;
+
+    let { rate, ranges, experimentType, rolloutType, requirements, priority, notes } = data[id];
+    let totalServers = 6700000;
+    let totalUsers = 196200000;
+    let count = ((experimentType === 0 ? totalServers : experimentType === 1 ? totalUsers : totalServers + totalUsers) / 100 * rate).toString().split('').reverse();
+    let fixed = [];
+    let group = [];
+    let timer = 0;
+
+    for (var digit of count) {
+        if (timer === 3) {
+            fixed.push(group.reverse());
+
+            group = [];
+            timer = 0;
+        };
+
+        group.push(digit);
+
+        timer++;
+    };
+
+    if (group.length > 0) fixed.push(group.reverse());
+
+    fixed = fixed.reverse().map(g => g.map(digit => digit).join('')).join('.');
+
+    let response = '';
+
+    if (rolloutType === -1) response = `<:admin:1105416266623234138> This feature's rollout has reverted due to some security issues. Restart date is unknown.`;
+    else if (rolloutType === 2) response = `<:ticket:1105416402782928927> This feature is very slowly rolling out to all ${experimentType === 0 ? 'servers' : experimentType === 1 ? 'users' : 'servers and users'} from old to new ones, this process may take a few months.`;
+    else if (rolloutType === 3) response = '';
 };
