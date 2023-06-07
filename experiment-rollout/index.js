2934ad316e1db711be8e4ea86abc6afcaf7115f7
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2023-06-07 16:47:36 +0300

    data override

--- experiment-rollout/index.js
+++ experiment-rollout/index.js
@@ -1,7 +1,7 @@
 const lastUpdate = '1685988000'; //Unix timestamp in seconds
 
-async function experimentRollout(command, override) { // `override` IS ONLY FOR DEVELOPMENT
-    const data = await fetch('https://raw.githubusercontent.com/discordexperimenthub/assyst-tags/f84018f872d12f4bba6a08ce7ad0d3fb8969d8df/experiment-rollout/data.json').then(res => res.json());
+async function experimentRollout(command, override = null) { // `override` IS ONLY FOR DEVELOPMENT
+    const data = await fetch(`https://raw.githubusercontent.com/discordexperimenthub/assyst-tags/${override ?? 'main'}/experiment-rollout/data.json`).then(res => res.json());
 
     let string = message.content.split(`${command} `)[1]?.toLowerCase() ?? '';
     let [id, subcommand, index] = string.split(' ');
