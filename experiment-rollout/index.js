ddc36e71cd144d1e57e42894dded8cbec7019c84
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2023-06-07 14:17:59 +0300

    real

--- experiment-rollout/index.js
+++ experiment-rollout/index.js
@@ -1,4 +1,5 @@
 const lastUpdate = '1685988000'; //Unix timestamp in seconds
+const data = fetch('https://assyst-tags-data.glitch.me/experiment-rollout').then(res => res.json());
 
 async function experimentRollout(command) {
     let string = message.content.split(`${command} `)[1]?.toLowerCase() ?? '';
